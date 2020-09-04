1755ca5bd20e97941f768246af234ae2c606124e
tuyen <camgiacmanh1999@gmail.com>
2020-09-05 03:15:11 +0700

    update code

--- server2.js
+++ server2.js
@@ -1,24 +1,14 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
+//if (process.env.NODE_ENV !== 'production') {
+  //  require('dotenv').config()
+ // }
   
   const express = require('express')
   const app = express()
   const bcrypt = require('bcrypt')
   const passport = require('passport')
   const flash = require('express-flash')
-  const session = require('express-session')
+  //const session = require('express-session')
   const methodOverride = require('method-override')
-  
-  //mongoose.connect(process.env.DATABASE_URL,{ useUnifiedTopology:true,useNewUrlParser:true})
-  //const db = mongoose.connection
-  //db.on('error',(error)=> console.error(error))
-  //db.once('open',()=>console.log('Connected on Database'))
-  
-  app.use(express.json())
-  
-  
-  
   
   const initializePassport = require('./passport-config')
   initializePassport(
@@ -32,52 +22,34 @@ if (process.env.NODE_ENV !== 'production') {
   app.set('view-engine', 'ejs')
   app.use(express.urlencoded({ extended: false }))
   app.use(flash())
-  app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-  }))
+  //app.use(session({
+ //   secret: process.env.SESSION_SECRET,
+  //  resave: false,
+  //  saveUninitialized: false
+  //}))
   app.use(passport.initialize())
   app.use(passport.session())
-  app.use(methodOverride('_method'))
+ app.use(methodOverride('_method'))
   
-  app.get('/', checkAuthenticated, (req, res) => {
+  app.get('/',  (req, res) => {
     res.render('home.ejs')
   })
   
-  app.get('/login', checkNotAuthenticated, (req, res) => {
+  app.get('/login',  (req, res) => {
     res.render('login.ejs')
   })
-  app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  
+  app.post('/login',passport.authenticate('local', {
     successRedirect: '/',
-    failureRedirect: '/login', failureFlash: true,
-    
+    failureRedirect: '/login',
+    failureFlash: true
+  }))
   
-  })
-  
-  )
-  app.post('/login',async (req,res)=>{
-    const user = users.find(user => user.name = req.body.name)
-    if(user == null){
-      return res.status(400).send('/')
-    }
-    try {
-      if(await bcrypt.compare(req.body.password,user.password)){
-        res.send('/')
-      }
-      else {
-        res.send('/login')
-      }
-    } catch {
-      res.status(500).send()
-    }
-  })
-  
-  app.get('/register', checkNotAuthenticated, (req, res) => {
+  app.get('/register',  (req, res) => {
     res.render('register.ejs')
   })
   
-  app.post('/register', checkNotAuthenticated, async (req, res) => {
+  app.post('/register',  async (req, res) => {
     try {
       const hashedPassword = await bcrypt.hash(req.body.password, 10)
       users.push({
@@ -93,32 +65,27 @@ if (process.env.NODE_ENV !== 'production') {
     console.log(users)
   })
   
-  app.delete('/logout', (req, res) => {
+app.delete('/logout',(req,res)=>{
     req.logOut()
     res.redirect('/login')
-  })
-  
-  function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next()
-    }
-  
-    res.redirect('/login')
+})
+
+  function checkAuthenticated(req,res,next){
+        if(req.isAuthenticated()){
+            return next;
+        }
+        res.redirect('/login')
   }
-  
-  function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return res.redirect('/')
+
+  /*function checkNotAuthenticated(req,res,next){
+    if(req.isAuthenticated()){
+        return res.redirect('/');
     }
     next()
-  }/*
-  const express = require('express')
-  const app = express()
-  const path = require('path')
-  app.use('/',(req,res)=>{
-    var duongDanFile = path.join(__dirname,'home.html')
-    res.sendFile(duongDanFile)
-  })*/
+}*/
+
+  
+  
   app.listen(process.env.PORT,function(){
-    
+      console.log('Server start on')
   })
