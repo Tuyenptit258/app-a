40c15ba7584cdc61ee1d784603b91fafe4f73a01
tuyen <camgiacmanh1999@gmail.com>
2020-09-05 01:23:09 +0700

    sua code

--- server2.js
+++ server2.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function(req,res){
-    res.json("hello tat ca moi nguoi ")
+    res.json("hello tat ca moi nguoi nhe ")
 })
 
 app.listen(process.env.PORT, function(){
