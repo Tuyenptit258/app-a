658f58281309788114205fcf1004993fcfad4565
tuyen <camgiacmanh1999@gmail.com>
2020-09-05 01:28:53 +0700

    update code

--- server2.js
+++ server2.js
@@ -1,9 +1,15 @@
 const express = require('express')
 
 const app = express()
+const path = require('path')
 
 app.get('/', function(req,res){
     res.json("hello tat ca moi nguoi nhe ")
+})
+
+app.get('/home', function(req,res){
+    var duongDanFile = path.join(__dirname, "home.html")
+    res.sendFile(duongDanFile)
 })
 
 app.listen(process.env.PORT, function(){
